fe1f84072167943fbde5c5987f0acaa87920e830
fy-yuhcan <fjtyuyuuki4180@gmail.com>
2025-02-01 20:12:49 +0900

    userテーブルからpasswordカラムを消して、redirectとcallbackメソッドを作成した

--- app/Http/Controllers/SocialiteController.php
+++ app/Http/Controllers/SocialiteController.php
@@ -3,8 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
-    //
+    //認証のリダイレクト処理
+    public function redirect($provider)
+    {
+        return Socialite::driver($provider)->redirect();
+    }
+
+    //認証のコールバック処理
+    public function callback($provider)
+    {
+        $user = Socialite::driver($provider)->user();
+        dd($user);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\SocialiteController;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//Google認証のルーティング
+Route::get('/redirect/{provider}', [SocialiteController::class, 'redirect']);
+Route::get('/callback/{provider}', [SocialiteController::class, 'callback']);
